44fb37e2cd4f9fb08b8e3997285bc04054745806
codeprakhar25 <prakharkhatri123@gmail.com>
2024-06-16 02:16:34 +0530

    Diary App Setup

--- app/src/main/java/com/example/portfolio/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/portfolio/ui/dashboard/DashboardFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.portfolio.databinding.FragmentDashboardBinding
@@ -61,5 +64,12 @@ class DashboardFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+}
+
+@Composable
+fun FilledButtonExample(onClick: () -> Unit) {
+    Button(onClick = { onClick() }) {
+        Text("Filled")
     }
 }
