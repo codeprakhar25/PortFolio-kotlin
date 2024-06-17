62c0b9202c8f60a48730b6f2720079530d996d1a
codeprakhar25 <prakharkhatri123@gmail.com>
2024-06-17 15:25:20 +0530

    feat: UI improved

--- app/src/main/java/com/example/portfolio/screens/WeatherScreen.kt
+++ app/src/main/java/com/example/portfolio/screens/WeatherScreen.kt
@@ -12,10 +12,12 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.portfolio.WeatherViewModel
 import com.example.portfolio.api.Response
 import com.example.portfolio.data.WeatherModel
@@ -70,79 +72,89 @@ fun WeatherScreen(viewModel: WeatherViewModel) {
     }
 }
 
-
 @Composable
 fun WeatherComponent(data: WeatherModel) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp)
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Icon(imageVector = Icons.Default.LocationOn, contentDescription = "location")
-            Text(text = "London United Kingdom", fontSize = 30.sp, modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location icon",
+                modifier = Modifier.size(40.dp)
+            )
+            Text(text = data.location.name, fontSize = 30.sp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = data.location.country, fontSize = 18.sp, color = Color.Gray)
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "11", fontSize = 50.sp, modifier = Modifier.padding(10.dp))
-            Text(text = "^ C", fontSize = 50.sp, modifier = Modifier.padding(10.dp))
-        }
-        Icon(
-            imageVector = Icons.Default.Done,
-            contentDescription = "",
-            modifier = Modifier.size(50.dp)
-        )
+
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = "Partly Cloudy",
-            color = Color.Cyan,
-            fontSize = 30.sp,
+            text = " ${data.current.temp_c} ° c",
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .background(Color.LightGray),
-            verticalArrangement = Arrangement.Center,
-            Alignment.CenterHorizontally
-        ) {
-            Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceAround) {
-                Column() {
-                    Text(text = "76", fontSize = 20.sp)
-                    Text(text = "Humidity", fontSize = 10.sp)
 
+        AsyncImage(
+            modifier = Modifier.size(160.dp),
+            model = "https:${data.current.condition.icon}".replace("64x64", "128x128"),
+            contentDescription = "Condition icon"
+        )
+        Text(
+            text = data.current.condition.text,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Card {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Humidity", data.current.humidity)
+                    WeatherKeyVal("Wind Speed", data.current.wind_kph + " km/h")
                 }
-                Column() {
-                    Text(text = "76", fontSize = 20.sp)
-                    Text(text = "Humidity", fontSize = 10.sp)
-
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("UV", data.current.uv)
+                    WeatherKeyVal("Participation", data.current.precip_mm + " mm")
                 }
-            }
-
-            Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceAround) {
-                Column() {
-                    Text(text = "76", fontSize = 20.sp)
-                    Text(text = "Humidity", fontSize = 10.sp)
-
-                }
-                Column() {
-                    Text(text = "76", fontSize = 20.sp)
-                    Text(text = "Humidity", fontSize = 10.sp)
-
-                }
-            }
-
-            Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceAround) {
-                Column() {
-                    Text(text = "76", fontSize = 20.sp)
-                    Text(text = "Humidity", fontSize = 10.sp)
-
-                }
-                Column() {
-                    Text(text = "76", fontSize = 20.sp)
-                    Text(text = "Humidity", fontSize = 10.sp)
-
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Local Time", data.location.localtime.split(" ")[1])
+                    WeatherKeyVal("Local Date", data.location.localtime.split(" ")[0])
                 }
             }
         }
+
+
+    }
+
+}
+
+@Composable
+fun WeatherKeyVal(key: String, value: String) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text(text = key, fontWeight = FontWeight.SemiBold, color = Color.Gray)
     }
 }
